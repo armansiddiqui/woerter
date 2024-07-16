a2a3c020f2da5431a408341e20945907b3160a7a
Arman <6960082+armansiddiqui@users.noreply.github.com>
2024-07-17 00:09:06 +0100

    using custom resize function instead of libs

--- main.js
+++ main.js
@@ -12,11 +12,26 @@ function printWord() {
     frontDiv.style.display="none";
     backDivText.style.display="block";
     backDivText.innerText = input.value;
-    fitty('h1');
+    resizeText(backDivText);
 }
 
 function goToFront() {
     frontDiv.style.display="block";
     backDivText.style.display="none";
     input.focus();
+}
+
+function resizeText(textElement) {
+    const parentWidth = window.innerWidth;
+    const parentHeight = window.innerHeight;
+    let fontSize = 2000; // Start with a large font size
+    textElement.style.fontSize = fontSize + 'px';
+
+    const bufferMargin = 0; // Adjust the buffer margin as needed
+    
+    // Reduce font size until the text fits within the parent width
+    while (textElement.offsetWidth + bufferMargin > parentWidth || textElement.offsetHeight + bufferMargin > parentHeight) {
+        fontSize-=0.5;
+        textElement.style.fontSize = fontSize + 'px';
+    }
 }
